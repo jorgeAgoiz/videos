feb2e500815c0ac9884af4e28a6adf6fa960c662
jorgeAgoiz <jorgeagoiz@gmail.com>
2021-03-09 20:57:23 +0100

    Search term engine

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import SearchBar from './SearchBar';
+import React from 'react'
+import SearchBar from './SearchBar'
+import youtube from '../apis/youtube'
 
 class App extends React.Component {
+  state = { videos: [] }
 
-    render () {
-        return (
-            <div className="ui container">
-                <SearchBar />
-            </div>
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+      },
+    })
 
-        );
-    }
-};
+    this.setState({ videos: response.data.items })
+    console.log(this.state.videos)
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <p>I have {this.state.videos.length} videos</p>
+      </div>
+    )
+  }
+}
+
+export default App
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,36 +1,37 @@
-import React from 'react';
+import React from 'react'
 
 class SearchBar extends React.Component {
-    state = { term: '' };//Lo inicializamos vacio
+  state = { term: '' } //Lo inicializamos vacio
 
-    onInputChange = (event) => {
-        //Cada vez que el texto dentro del input cambia actualizamos el estado
-        this.setState({ term: event.target.value });
-    };
+  onInputChange = (event) => {
+    //Cada vez que el texto dentro del input cambia actualizamos el estado
+    this.setState({ term: event.target.value })
+  }
 
-    onFormSubmit = (event) => {
-        event.preventDefault();//Para que el formulario no envie automaticamente
+  onFormSubmit = (event) => {
+    event.preventDefault() //Para que el formulario no envie automaticamente
 
-        //TODO: Asegurarnos de que llamamos el callback desde el componente padre
-    };
+    //TODO: Asegurarnos de que llamamos el callback desde el componente padre
+    this.props.onFormSubmit(this.state.term)
+  }
 
-    render () {
-        return (
-            /* Usaremos Semantic UI como CSS Framework */
-            <div className="search-bar ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <label>Video Search</label>
-                        <input
-                            type="text"
-                            value={this.state.term}
-                            onChange={this.onInputChange}
-                        />
-                    </div>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      /* Usaremos Semantic UI como CSS Framework */
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default SearchBar;
+export default SearchBar
